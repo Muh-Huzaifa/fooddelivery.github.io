60414aca4e6839efe7e775fb7384e72ae37fb234
Muh-Huzaifa <muhammadhuzaifa3898@gmail.com>
2024-09-06 17:47:40 +0500

    upadated

---
+++ script.js
@@ -0,0 +1,39 @@
+
+let random = Math.random()
+console.log(random)
+function calc() {
+    let a = prompt("Enter first number")
+    let c = prompt("Enter operation")
+    let b = prompt("Enter second number")
+    
+    let obj = {
+        "+": "-",
+        "*": "+",
+        "-": "/",
+        "/": "**",
+    }
+    
+    
+    
+    if (random > 0.1) {
+        // Perform correct calculation
+        console.log(`The result is ${a} ${c} ${b}`)
+        alert(`The result is ${eval(`${a} ${c} ${b}`)}`)
+        
+    }
+    
+    else {
+        // Perform wrong calculation
+        c = obj[c]
+        alert(`The result is ${eval(`${a} ${c} ${b}`)}`) 
+    
+    }
+        
+}
+
+// Get the button element by its ID
+const button = document.getElementById('myButton');
+
+// Add an event listener to the button
+button.addEventListener('click', calc);
+
---
+++ script2.js
@@ -0,0 +1,51 @@
+
+function randomgeneation() {
+    let rand = Math.random()
+    let first, second, third;
+    // 0 0.33 0.66 1
+
+
+    // Lets generate the first word
+    if(rand<0.33){
+        first = "Crazy"
+    }
+    else if(rand<0.66 && rand>=0.33){
+        first = "Amazing"
+    }
+    else{
+        first = "Fire"
+    }
+
+    // Lets generate the second word
+    rand = Math.random()
+    if(rand<0.33){
+        second = "Engine"
+    }
+    else if(rand<0.66 && rand>=0.33){
+        second = "Foods"
+    }
+    else{
+        second = "Garments"
+    }
+
+    // Lets generate the third word
+    rand = Math.random()
+    if(rand<0.33){
+        third = "Bros"
+    }
+    else if(rand<0.66 && rand>=0.33){
+        third = "Limited"
+    }
+    else{
+        third = "Hub"
+    }
+
+    alert(`The Random generated word is :${first} ${second} ${third}`)
+}
+
+// Get the button element by its ID
+const button1 = document.getElementById('randomGen');
+
+// Add an event listener to the button
+button1.addEventListener('click', randomgeneation);
+
